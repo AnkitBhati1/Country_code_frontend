d62cf6c8cfe6c7d1eedfbd45db2312deea2d06ea
AnkitBhati1 <bhatiankit9460@gmail.com>
2022-07-28 12:49:28 +0530

    send api fixed js call fixed

--- public/hello.js
+++ public/hello.js
@@ -1,46 +1,17 @@
-// const api_url = "https://httpbin.org/ip"
-
 function getData() {
-
     fetch("https://httpbin.org/ip")
-
         .then(response => response.json())
-
         .then(data => {
-
-            // make api call to http://ip-api.com/json/$ip to get user location
-
             const ip_api_url = "https://ipapi.co/" + data.origin + "/json/"
-
             fetch(ip_api_url)
-
                 .then(response => response.json())
-
                 .then(data => {
-
-                    //   send this data to server
-                    console.log(data)
-
                     fetch('/api/hello', {
-
                         method: 'POST',
-
                         body: JSON.stringify(data)
-
-                    }).then(response => response.json())
-
-                        .then(data => {
-
-                            console.log(data)
-
-                        })
-
+                    })
                 })
-
         })
-
 }
-
 // call the function to make api call
-
 getData()
